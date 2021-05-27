11da441b0e771d726319ad1f6b6916b32d17f6a0
Valentino Giudice <valentino.giudice96@gmail.com>
2021-05-27 17:30:40 +0200

    M

--- parallel01_smn.cu
+++ parallel01_smn.cu
@@ -195,9 +195,9 @@ void blur(int n, int width, int height, stbi_uc *img_d, unsigned short *aux1_d, 
 	pascal(filter2, 15);
 	dim3 blocks((width + 31) / 32, (height + 31) / 32, 3);
 	dim3 blocks2((width + 31) / 32, (height + 31) / 32 / NBLOCK, 3);
-	dim3 blocks3((width + 1023) / 1024, height, 3);
+	dim3 blocks3((width + 255) / 256, height, 3);
 	dim3 threadsPerBlock(32, 32, 1);
-	dim3 threadsPerBlock2(1024, 1, 1);
+	dim3 threadsPerBlock2(256, 1, 1);
 	cudaMalloc(&filter1_d, sizeof(int) * n_init);
 	cudaMalloc(&filter2_d, sizeof(int) * 15);
 	cudaMemcpy(filter1_d, filter1, sizeof(int) * n_init, cudaMemcpyHostToDevice);
