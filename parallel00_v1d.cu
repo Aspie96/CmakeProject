7c7f20816c4947b6c87285920ef657ee86c4db23
Valentino Giudice <valentino.giudice96@gmail.com>
2021-08-01 23:41:53 +0200

    M

--- parallel00_v1d.cu
+++ parallel00_v1d.cu
@@ -26,8 +26,8 @@ void blur(int width, int height) {
 	int i;
 	size_t img1_pitch, img2_pitch;
 	unsigned short *restrict img1, *restrict img2;
-	dim3 blocks((width + 31) / 32, (height + 31) / 32, 3);
-	dim3 threadsPerBlock(32, 32, 1);
+	dim3 blocks(3, (width + 31) / 32, (height + 31) / 32);
+	dim3 threadsPerBlock(1, 32, 32);
 
 	cudaMallocPitch((void **)&img1, &img1_pitch, sizeof(unsigned short) * width, height * 3);
 	img1_pitch /= sizeof(unsigned short);
