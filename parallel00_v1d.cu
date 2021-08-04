8286320b9de6404324df5847067da03ddf1b2c1e
Valentino Giudice <valentino.giudice96@gmail.com>
2021-08-04 04:49:56 +0200

    M

--- parallel00_v1d.cu
+++ parallel00_v1d.cu
@@ -7,9 +7,9 @@
 __global__
 void kernel1b(const unsigned short *restrict img, int width, int height, size_t result_pitch, size_t img_pitch, int n, unsigned short *restrict result) {
 	int i, j, z, k, l, c;
-	z = blockIdx.x;
-	i = blockIdx.y * blockDim.y + threadIdx.y;
-	j = blockIdx.z * blockDim.z + threadIdx.z;
+	i = blockIdx.x * blockDim.x + threadIdx.x;
+	j = blockIdx.y * blockDim.y + threadIdx.y;
+	z = blockIdx.z;
 	if(i < width && j < height) {
 		c = 0;
 		for(k = 0; k < n; k++) {
@@ -26,8 +26,8 @@ void blur(int width, int height) {
 	int i;
 	size_t img1_pitch, img2_pitch;
 	unsigned short *restrict img1, *restrict img2;
-	dim3 blocks(3, (width + 31) / 32, (height + 31) / 32);
-	dim3 threadsPerBlock(1, 32, 32);
+	dim3 blocks((width + 31) / 32, (height + 31) / 32, 3);
+	dim3 threadsPerBlock(32, 32, 1);
 
 	cudaMallocPitch((void **)&img1, &img1_pitch, sizeof(unsigned short) * width, height * 3);
 	img1_pitch /= sizeof(unsigned short);
