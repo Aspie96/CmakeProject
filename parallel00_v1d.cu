758692573510ee93e20182a1fa09f52b3c7cde84
Valentino Giudice <valentino.giudice96@gmail.com>
2021-08-05 18:22:56 +0200

    M

--- parallel00_v1d.cu
+++ parallel00_v1d.cu
@@ -21,7 +21,7 @@ void pascal(int *p, int n) {
 //#endif
 
 __global__
-void kernel1b1(const unsigned short *restrict img, int width, int height, size_t result_pitch, size_t img_pitch, int n, const int *restrict filter, unsigned short *restrict result) {
+void kernel1b1(const unsigned short *restrict img, int width, int height, size_t result_pitch, size_t img_pitch, int n, unsigned short *restrict result) {
 	int i, j, z, k, l, c, m;
 	z = blockIdx.x;
 	i = blockIdx.y * blockDim.y + threadIdx.y;
@@ -38,11 +38,11 @@ void kernel1b1(const unsigned short *restrict img, int width, int height, size_t
 			if(0 <= l && l < width) {
 				m += img[(z * height + j) * img_pitch + l];
 			}
-			c += filter[k] * m;
+			c += m;
 		}
 		l = i + k - n / 2;
 		if(0 <= l && l < width) {
-			c += filter[k] * img[(z * height + j) * img_pitch + l];
+			c += img[(z * height + j) * img_pitch + l];
 		}
 		result[(z * height + j) * result_pitch + i] = APPROX_DIVIDE2(c, n - 1);
 	}
@@ -77,7 +77,7 @@ void kernel2a1(const unsigned short *restrict img, int width, int height, size_t
 }
 
 __global__
-void kernel1b3(const unsigned short *restrict img, int width, int height, size_t result_pitch, size_t img_pitch, int n, const int *restrict filter, unsigned short *restrict result) {
+void kernel1b3(const unsigned short *restrict img, int width, int height, size_t result_pitch, size_t img_pitch, int n, unsigned short *restrict result) {
 	int i, j, z, k, l, c, m;
 	z = blockIdx.x;
 	i = blockIdx.y * blockDim.y + threadIdx.y;
@@ -94,11 +94,11 @@ void kernel1b3(const unsigned short *restrict img, int width, int height, size_t
 			if(0 <= l && l < width) {
 				m += img[(j * img_pitch + l * 3) + z];
 			}
-			c += filter[k] * m;
+			c += m;
 		}
 		l = i + k - n / 2;
 		if(0 <= l && l < width) {
-			c += filter[k] * img[(j * img_pitch + l * 3) + z];
+			c += img[(j * img_pitch + l * 3) + z];
 		}
 		result[(j * result_pitch + i * 3) + z] = APPROX_DIVIDE2(c, n - 1);
 	}
@@ -133,23 +133,18 @@ void kernel2a3(const unsigned short *restrict img, int width, int height, size_t
 }
 
 void blur1(int width, int height) {
-	int i, *restrict filter2, *restrict filter2_d;
+	int i;
 	size_t img1_pitch, img2_pitch;
 	unsigned short *restrict img1, *restrict img2;
 	dim3 blocks(3, (width + 31) / 32, (height + 31) / 32);
 	dim3 threadsPerBlock(1, 32, 32);
-
-	filter2 = (int *)malloc(sizeof(int) * 9);
-	pascal(filter2, 17);
-	cudaMalloc((void **)&filter2_d, sizeof(int) * 9);
-	cudaMemcpy(filter2_d, filter2, sizeof(int) * 9, cudaMemcpyHostToDevice);
 
 	cudaMallocPitch((void **)&img1, &img1_pitch, sizeof(unsigned short) * width, height * 3);
 	img1_pitch /= sizeof(unsigned short);
 	cudaMallocPitch((void **)&img2, &img2_pitch, sizeof(unsigned short) * width, height * 3);
 	img2_pitch /= sizeof(unsigned short);
 	for(i = 0; i < 1000; i++) {
-		kernel1b1 << <blocks, threadsPerBlock >> > (img1, width, height, img2_pitch, img1_pitch, 17, filter2_d, img2);
+		kernel1b1 << <blocks, threadsPerBlock >> > (img1, width, height, img2_pitch, img1_pitch, 17, img2);
 	}
 	cudaFree(img1);
 	cudaFree(img2);
@@ -157,23 +152,18 @@ void blur1(int width, int height) {
 }
 
 void blur3(int width, int height) {
-	int i, *restrict filter2, *restrict filter2_d;
+	int i;
 	size_t img1_pitch, img2_pitch;
 	unsigned short *restrict img1, *restrict img2;
 	dim3 blocks(3, (width + 31) / 32, (height + 31) / 32);
 	dim3 threadsPerBlock(1, 32, 32);
-
-	filter2 = (int *)malloc(sizeof(int) * 9);
-	pascal(filter2, 17);
-	cudaMalloc((void **)&filter2_d, sizeof(int) * 9);
-	cudaMemcpy(filter2_d, filter2, sizeof(int) * 9, cudaMemcpyHostToDevice);
 
 	cudaMallocPitch((void **)&img1, &img1_pitch, sizeof(unsigned short) * width * 3, height);
 	img1_pitch /= sizeof(unsigned short);
 	cudaMallocPitch((void **)&img2, &img2_pitch, sizeof(unsigned short) * width * 3, height);
 	img2_pitch /= sizeof(unsigned short);
 	for(i = 0; i < 1000; i++) {
-		kernel1b3 << <blocks, threadsPerBlock >> > (img1, width, height, img2_pitch, img1_pitch, 17, filter2_d, img2);
+		kernel1b3 << <blocks, threadsPerBlock >> > (img1, width, height, img2_pitch, img1_pitch, 17, img2);
 	}
 	cudaFree(img1);
 	cudaFree(img2);
