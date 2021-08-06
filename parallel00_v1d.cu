047a8c11c072b6be9d53e5a0f2518f517d78eed0
Valentino Giudice <valentino.giudice96@gmail.com>
2021-08-06 17:11:28 +0200

    M

--- parallel00_v1d.cu
+++ parallel00_v1d.cu
@@ -82,7 +82,7 @@ void blur(int width, int height) {
 	img2_pitch /= sizeof(unsigned short);
 	for(i = 0; i < 1000; i++) {
 		// CALL kerne1 OR kernel2
-		kernel2 << <blocks, threadsPerBlock >> > (img2, img1, width, height, img2_pitch, img1_pitch, 17, filter_d);
+		kernel1 << <blocks, threadsPerBlock >> > (img2, img1, width, height, img2_pitch, img1_pitch, 17, filter_d);
 	}
 	cudaFree(img1);
 	cudaFree(img2);
