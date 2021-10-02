0aff00c04211ffa7743dc36d704fc50e0fffaa81
Valentino Giudice <valentino.giudice96@gmail.com>
2021-10-02 19:01:06 +0200

    M

--- parallel00_v4.cu
+++ parallel00_v4.cu
@@ -11,7 +11,7 @@
 #define APPROX_DIVIDE1(A, B) (S_R_SHIFT(A, B) + (S_R_SHIFT(A, (B) - 1) & 1))
 #define APPROX_DIVIDE2(A, B) (((A) >> (B)) + (((A) >> ((B) - 1)) & 1))
 #ifndef N
-#define N 9
+#define N 13
 #endif
 #ifndef WIDTH
 #define WIDTH 0
